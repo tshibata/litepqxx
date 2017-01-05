7f7debca49576d6fc806a47851bb92d92e37efb5
tshibata <tshibata@boutem.com>
2017-01-05 19:12:19 +0100

    redundant cases

--- src/error.cxx
+++ src/error.cxx
@@ -17,32 +17,32 @@ void check_sqlite3(int code) {
 		throw integrity_constraint_violation();
 	case SQLITE_ERROR:
 		throw syntax_error();
-	case SQLITE_INTERNAL:
-	case SQLITE_PERM:
-	case SQLITE_ABORT:
-	case SQLITE_BUSY:
-	case SQLITE_LOCKED:
-	case SQLITE_READONLY:
-	case SQLITE_INTERRUPT:
-	case SQLITE_IOERR:
-	case SQLITE_CORRUPT:
-	case SQLITE_NOTFOUND:
-	case SQLITE_CANTOPEN:
-	case SQLITE_PROTOCOL:
-	case SQLITE_EMPTY:
-	case SQLITE_SCHEMA:
-	case SQLITE_TOOBIG:
-	case SQLITE_MISMATCH:
-	case SQLITE_MISUSE:
-	case SQLITE_NOLFS:
-	case SQLITE_AUTH:
-	case SQLITE_FORMAT:
-	case SQLITE_RANGE:
-	case SQLITE_NOTADB:
-	case SQLITE_NOTICE:
-	case SQLITE_WARNING:
-	case SQLITE_ROW:
-	case SQLITE_DONE:
+	//case SQLITE_INTERNAL:
+	//case SQLITE_PERM:
+	//case SQLITE_ABORT:
+	//case SQLITE_BUSY:
+	//case SQLITE_LOCKED:
+	//case SQLITE_READONLY:
+	//case SQLITE_INTERRUPT:
+	//case SQLITE_IOERR:
+	//case SQLITE_CORRUPT:
+	//case SQLITE_NOTFOUND:
+	//case SQLITE_CANTOPEN:
+	//case SQLITE_PROTOCOL:
+	//case SQLITE_EMPTY:
+	//case SQLITE_SCHEMA:
+	//case SQLITE_TOOBIG:
+	//case SQLITE_MISMATCH:
+	//case SQLITE_MISUSE:
+	//case SQLITE_NOLFS:
+	//case SQLITE_AUTH:
+	//case SQLITE_FORMAT:
+	//case SQLITE_RANGE:
+	//case SQLITE_NOTADB:
+	//case SQLITE_NOTICE:
+	//case SQLITE_WARNING:
+	//case SQLITE_ROW:
+	//case SQLITE_DONE:
 	default:
 		throw internal_error();
 	}
